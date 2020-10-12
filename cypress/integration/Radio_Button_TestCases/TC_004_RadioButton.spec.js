5f1ef3542805167550f7d189e6b557c2bb49dcb1
Raghavendra Prasad R <Raghavendra.R@EPS-SCL-35.excelindia.com>
2020-10-12 14:50:20 +0530

    Radio Button Test Case

--- cypress/integration/Radio_Button_TestCases/TC_004_RadioButton.spec.js
+++ cypress/integration/Radio_Button_TestCases/TC_004_RadioButton.spec.js
@@ -2,58 +2,61 @@
 
 const radiobuttonLocators = require('../../pageObjects/radiobuttonPageObjects.json')
 
+import utilMethods from '../../utilMethods/util.js'
+const utilityMethods = new utilMethods();
+
 describe("Radio Button Actions",()=>{
+
+    before(()=>{
+        //Open the form page URL
+        cy.visit("https://www.demoqa.com/automation-practice-form");
+    });
 
     it("Clicking on Radio button",()=>{
 
-        cy.visit("https://www.demoqa.com/automation-practice-form");
+        //Filling the form with all the feilds
+        cy.get(radiobuttonLocators.firstNameTextbox).type(utilityMethods.randomString("5","lowerCase"));
 
-        cy.get("input[id='firstName']").type('Amar');
+        cy.get(radiobuttonLocators.lastNameTextbox).type(utilityMethods.randomString("8","upperCase"));
 
-        cy.get("#lastName").type('Nanjangud');
+        cy.get(radiobuttonLocators.userEmailTextbox).type(utilityMethods.randomString("4","lowerCase")+'@gmail.com');
 
+        cy.get(radiobuttonLocators.maleRadiobutton).click();
 
-        cy.get("#userEmail").type('Amar@gmail.com');
+        cy.get(radiobuttonLocators.userNumberTextbox).type(utilityMethods.randomString("10","numeric"));
 
+        cy.get(radiobuttonLocators.dateOfBirthTextbox).click();
 
-        cy.get("label[for='gender-radio-1']").click();
-
-
-        cy.get("#userNumber").type('8095149954');
-
-        cy.get("#dateOfBirthInput").click();
-
-        cy.get(".react-datepicker__month-select").select('February');
+        cy.get(radiobuttonLocators.monthSelectorDropdown).select('February');
         
+        cy.get(radiobuttonLocators.yearSelectorDropdown).select('1994');
 
-        cy.get(".react-datepicker__year-select").select('1994');
-
-        cy.get("div[aria-label='Choose Sunday, February 20th, 1994']").click();
-
+        cy.get(radiobuttonLocators.dateValue).click();
 
         let subjects=["Maths","Physics"]
 
         for(let i=0;i<subjects.length;i++){
 
-            cy.get('.subjects-auto-complete__value-container.subjects-auto-complete__value-container--is-multi.css-1hwfws3').type(subjects[i]+"{enter}");
+            cy.get(radiobuttonLocators.subjectTextbox).type(subjects[i]+"{enter}");
 
         }
 
-        cy.get("label[for='hobbies-checkbox-1']").click();
+        cy.get(radiobuttonLocators.hobbiesCheckbox).click();
         
-        cy.get("#currentAddress").type("Tester Tester Tester");
+        cy.get(radiobuttonLocators.addressTextbox).type(utilityMethods.randomString("8","lowerCase"));
 
-        cy.get('#state > .css-yk16xz-control > .css-1hwfws3').type('Rajas{enter}')
+        cy.get(radiobuttonLocators.stateDropdown).type('Rajas{enter}')
 
-        cy.get('#stateCity-wrapper > :nth-child(3)').type('Jai{enter}')
+        cy.get(radiobuttonLocators.cityDropdown).type('Jai{enter}')
 
-        cy.get('#submit').click()
+        //Submitting the Filled form
+        cy.get(radiobuttonLocators.submitButton).click()
         
-        cy.get('.modal-header').should('contain','Thanks for submitting the form');
+        //Validating the Success Message of the form
+        cy.get(radiobuttonLocators.successMessageHeader).should('contain','Thanks for submitting the form');
 
-
-
-
+        //Closing the Popup Window
+        cy.get(radiobuttonLocators.closeButton).click();
 
     });
 
